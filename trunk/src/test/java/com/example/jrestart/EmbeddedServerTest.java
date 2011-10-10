b90b7216f2bf320852df36c45ce725abbd411db4
alex.irimiea@gmail.com <alex.irimiea@gmail.com@f5cba435-cfed-1b4a-e9fa-8e5f21bc15f7>
2011-10-10 13:26:32 +0000

    added also methods for HTTP PUT and HTTP DELETE

--- trunk/src/test/java/com/example/jrestart/EmbeddedServerTest.java
+++ trunk/src/test/java/com/example/jrestart/EmbeddedServerTest.java
@@ -2,8 +2,10 @@ package com.example.jrestart;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
@@ -14,6 +16,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import javax.ws.rs.core.Response;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -35,6 +38,9 @@ public class EmbeddedServerTest {
 
     public static final String POST_ADD_EMPLOYEE_JSON_URL = URL + "/post/";
 
+    public static final String PUT_UPDATE_EMPLOYEE_JSON_URL = URL + "/put/";
+
+    public static final String DELETE_REMOVE_EMPLOYEE_JSON_URL = URL + "/delete/3";
 
     private static Server server;
 
@@ -72,7 +78,7 @@ public class EmbeddedServerTest {
             getRequest.addHeader("accept", mimeType);
             HttpResponse response = httpClient.execute(getRequest);
 
-            if (response.getStatusLine().getStatusCode() != 200) {
+            if (response.getStatusLine().getStatusCode() != Response.Status.OK.getStatusCode()) {
                 logger.error("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
                 Assert.fail();
             }
@@ -111,7 +117,7 @@ public class EmbeddedServerTest {
 
             HttpResponse response = httpClient.execute(postRequest);
 
-            if (response.getStatusLine().getStatusCode() != 201) {
+            if (response.getStatusLine().getStatusCode() != Response.Status.CREATED.getStatusCode()) {
                 logger.error("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
                 Assert.fail();
             }
@@ -125,6 +131,88 @@ public class EmbeddedServerTest {
                 logger.info(output);
             }
             logger.info("\n");
+        } catch (ClientProtocolException e) {
+            logger.error("An exception has occurred", e);
+            Assert.fail();
+        } catch (IOException e) {
+            logger.error("An exception has occurred", e);
+            Assert.fail();
+        } finally {
+            if (httpClient != null) {
+                httpClient.getConnectionManager().shutdown();
+            }
+        }
+    }
+
+    private void executePUTRequest(String url, String input) {
+        DefaultHttpClient httpClient = null;
+        try {
+            httpClient = new DefaultHttpClient();
+            HttpPut putRequest = new HttpPut(url);
+
+            StringEntity inputEntity = new StringEntity(input);
+            inputEntity.setContentType("application/json");
+            putRequest.setEntity(inputEntity);
+
+            HttpResponse response = httpClient.execute(putRequest);
+
+            if (response.getStatusLine().getStatusCode() != Response.Status.CREATED.getStatusCode()) {
+                logger.error("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+                Assert.fail();
+            }
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+            String output;
+            logger.info("Response for URL: " + url);
+            while ((output = br.readLine()) != null) {
+                logger.info(output);
+            }
+            logger.info("\n");
+        } catch (ClientProtocolException e) {
+            logger.error("An exception has occurred", e);
+            Assert.fail();
+        } catch (IOException e) {
+            logger.error("An exception has occurred", e);
+            Assert.fail();
+        } finally {
+            if (httpClient != null) {
+                httpClient.getConnectionManager().shutdown();
+            }
+        }
+    }
+
+    private void executeDELETERequest(String url, String mimeType) {
+        DefaultHttpClient httpClient = null;
+        try {
+            httpClient = new DefaultHttpClient();
+            HttpDelete deleteRequest = new HttpDelete(url);
+            deleteRequest.addHeader("accept", mimeType);
+            HttpResponse response = httpClient.execute(deleteRequest);
+
+//            StringEntity inputEntity = new StringEntity(input);
+//            inputEntity.setContentType("application/json");
+//            deleteRequest.setEntity(inputEntity);
+//            HttpResponse response = httpClient.execute(deleteRequest);
+
+            if (response.getStatusLine().getStatusCode() != Response.Status.OK.getStatusCode() &&
+                response.getStatusLine().getStatusCode() != Response.Status.NO_CONTENT.getStatusCode()) {
+                logger.error("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+                Assert.fail();
+            }
+
+            if (response.getStatusLine().getStatusCode() == Response.Status.OK.getStatusCode()) {
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader((response.getEntity().getContent())));
+
+                String output;
+                logger.info("Response for URL: " + url);
+                while ((output = br.readLine()) != null) {
+                    logger.info(output);
+                }
+                logger.info("\n");
+            }
         } catch (ClientProtocolException e) {
             logger.error("An exception has occurred", e);
             Assert.fail();
@@ -169,8 +257,18 @@ public class EmbeddedServerTest {
     }
 
     @Test
-    public void testAddEmployeeJSON() {
+    public void testAddUpdateDeleteEmployeeJSON() {
+        //add new employee
         executePOSTRequest(POST_ADD_EMPLOYEE_JSON_URL, "{\"employeeId\":3,\"employeeName\":\"Maria\",\"job\":\"Tester\"}");
+        executeGETRequest(GET_LIST_EMPLOYEES_JSON_URL, "application/json");
+
+        //update newly added employee
+        executePUTRequest(PUT_UPDATE_EMPLOYEE_JSON_URL, "{\"employeeId\":3,\"employeeName\":\"Michael\",\"job\":\"QA Engineer\"}");
+        executeGETRequest(GET_LIST_EMPLOYEES_JSON_URL, "application/json");
+
+        //delete newly added employee
+        executeDELETERequest(DELETE_REMOVE_EMPLOYEE_JSON_URL, "text/plain");
+        executeGETRequest(GET_LIST_EMPLOYEES_JSON_URL, "application/json");
     }
 
 }
