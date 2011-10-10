5f263db5cbe5fb6cf0d625bac36d0fa22634f7f1
alex.irimiea@gmail.com <alex.irimiea@gmail.com@f5cba435-cfed-1b4a-e9fa-8e5f21bc15f7>
2011-10-10 09:29:42 +0000

    added POST method and updated the unit test accordingly

--- trunk/src/test/java/com/example/jrestart/EmbeddedServerTest.java
+++ trunk/src/test/java/com/example/jrestart/EmbeddedServerTest.java
@@ -3,6 +3,8 @@ package com.example.jrestart;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
 import org.mortbay.jetty.Server;
@@ -24,12 +26,14 @@ public class EmbeddedServerTest {
 
     public static final String URL = "http://localhost:8090/rest/employees";
 
-    public static final String HELLO_URL = URL + "/hello";
-    public static final String ECHO_URL = URL + "/echo/HelloWorld";
-    public static final String LIST_EMPLOYEES_URL = URL + "/employees";
+    public static final String GET_HELLO_URL = URL + "/hello";
+    public static final String GET_ECHO_URL = URL + "/echo/HelloWorld";
+    public static final String GET_LIST_EMPLOYEES_URL = URL + "/employees";
     public static final String GET_EMPLOYEE_URL = URL + "/employee/1";
-    public static final String LIST_EMPLOYEES_JSON_URL = URL + "/json/employees";
+    public static final String GET_LIST_EMPLOYEES_JSON_URL = URL + "/json/employees";
     public static final String GET_EMPLOYEE_JSON_URL = URL + "/json/employee/1";
+
+    public static final String POST_ADD_EMPLOYEE_JSON_URL = URL + "/post/";
 
 
     private static Server server;
@@ -60,7 +64,7 @@ public class EmbeddedServerTest {
         }
     }
 
-    private void getRequest(String url, String mimeType) {
+    private void executeGETRequest(String url, String mimeType) {
         DefaultHttpClient httpClient = null;
         try {
             httpClient = new DefaultHttpClient();
@@ -77,7 +81,46 @@ public class EmbeddedServerTest {
                     new InputStreamReader((response.getEntity().getContent())));
 
             String output;
-            logger.info("Response for URL: "+url);
+            logger.info("Response for URL: " + url);
+            while ((output = br.readLine()) != null) {
+                logger.info(output);
+            }
+            logger.info("\n");
+        } catch (ClientProtocolException e) {
+            logger.error("An exception has occurred", e);
+            Assert.fail();
+        } catch (IOException e) {
+            logger.error("An exception has occurred", e);
+            Assert.fail();
+        } finally {
+            if (httpClient != null) {
+                httpClient.getConnectionManager().shutdown();
+            }
+        }
+    }
+
+    private void executePOSTRequest(String url, String input) {
+        DefaultHttpClient httpClient = null;
+        try {
+            httpClient = new DefaultHttpClient();
+            HttpPost postRequest = new HttpPost(url);
+
+            StringEntity inputEntity = new StringEntity(input);
+            inputEntity.setContentType("application/json");
+            postRequest.setEntity(inputEntity);
+
+            HttpResponse response = httpClient.execute(postRequest);
+
+            if (response.getStatusLine().getStatusCode() != 201) {
+                logger.error("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+                Assert.fail();
+            }
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+            String output;
+            logger.info("Response for URL: " + url);
             while ((output = br.readLine()) != null) {
                 logger.info(output);
             }
@@ -97,32 +140,37 @@ public class EmbeddedServerTest {
 
     @Test
     public void testHello() {
-        getRequest(HELLO_URL, "text/plain");
+        executeGETRequest(GET_HELLO_URL, "text/plain");
     }
 
     @Test
     public void testEcho() {
-        getRequest(ECHO_URL, "text/plain");
+        executeGETRequest(GET_ECHO_URL, "text/plain");
     }
 
     @Test
     public void testListEmployees() {
-        getRequest(LIST_EMPLOYEES_URL, "application/xml");
+        executeGETRequest(GET_LIST_EMPLOYEES_URL, "application/xml");
     }
 
     @Test
     public void testGetEmployee() {
-        getRequest(GET_EMPLOYEE_URL, "application/xml");
+        executeGETRequest(GET_EMPLOYEE_URL, "application/xml");
     }
 
     @Test
     public void testListEmployeesJSON() {
-        getRequest(LIST_EMPLOYEES_JSON_URL, "application/json");
+        executeGETRequest(GET_LIST_EMPLOYEES_JSON_URL, "application/json");
     }
 
     @Test
     public void testGetEmployeeJSON() {
-        getRequest(GET_EMPLOYEE_JSON_URL, "application/json");
+        executeGETRequest(GET_EMPLOYEE_JSON_URL, "application/json");
+    }
+
+    @Test
+    public void testAddEmployeeJSON() {
+        executePOSTRequest(POST_ADD_EMPLOYEE_JSON_URL, "{\"employeeId\":3,\"employeeName\":\"Maria\",\"job\":\"Tester\"}");
     }
 
 }
